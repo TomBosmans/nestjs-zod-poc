11d493c0a30ecb2e39a09494aaf93a897285e3bc
tbosmans <tom.94.bosmans@gmail.com>
2022-09-13 12:41:00 +0200

    Update the prettier config.

--- backend/src/modules/config/config.module.ts
+++ backend/src/modules/config/config.module.ts
@@ -1,9 +1,9 @@
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from "@nestjs/config"
 
 export const DynamicConfigModule = ConfigModule.forRoot({
-  envFilePath: [`.env.${process.env.NODE_ENV ?? ''}`],
+  envFilePath: [`.env.${process.env.NODE_ENV ?? ""}`],
   validationOptions: {
     allowUnknown: false,
     abortEarly: true,
   },
-});
+})
